798f75dc23a77dafa117936c18ad9509bbe931d8
Soundarya2398 <soundaryak949@gmail.com>
2021-10-14 19:33:02 +0530

    Pattern2 Programm

---
+++ Pattern2.java
@@ -0,0 +1,18 @@
+class Pattern2{
+
+public static void main(String[] args){
+int row=3;
+int star=3;
+for(int i=1;i<=row;i++){
+for(int j=1;j<=star;j++){
+System.out.print("*"+" ");
+
+
+}
+System.out.println();
+star--;
+}
+
+
+}
+}
