8c2ef09e684ecef977598d37811def3bdc218fde
Soundarya2398 <soundaryak949@gmail.com>
2021-10-17 20:18:21 +0530

    5th pattern

---
+++ pattern5.java
@@ -0,0 +1,19 @@
+class Pattern5{
+public static void main(String[] args){
+
+int row=5;
+int star=1;
+     for(int i=1;i<=row;i++){
+         for(int j=1;j<=star;j++){
+             System.out.print("*");
+}
+System.out.println();
+if(i<=row/2){
+star++;
+}
+else{
+star--;
+}
+}
+}
+}
