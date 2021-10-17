a2e7992c7683145e2043919428f98114083f6489
Soundarya2398 <soundaryak949@gmail.com>
2021-10-17 20:22:58 +0530

    7th pattern

---
+++ Pattern7.java
@@ -0,0 +1,29 @@
+class Pattern7{
+public static void main(String[] args){
+
+int row=5;
+int star=1;
+int space=2;
+     for(int i=1;i<=row;i++){
+         for(int j=1;j<=space;j++){
+             System.out.print(" ");
+}
+for(int j=1;j<=star;j++){
+System.out.print("*");
+}
+
+System.out.println();
+if(i<=row/2){
+star+=2;
+space--;
+}else{
+space++;
+star-=2;
+}
+
+}
+
+
+}
+
+}
